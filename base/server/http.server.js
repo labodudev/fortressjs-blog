ddd3e7613b2463e5e83cd74d2cb46f35fa9d65e1
adrien-thierry <adrien.thierry@live.fr>
2016-04-17 15:23:02 +0200

    Update http.server.js
    Add req.SERVER to get current server

--- base/server/http.server.js
+++ base/server/http.server.js
@@ -30,7 +30,8 @@ function HttpServer(srv)
             req.srv = srv;
             req.srvId = srv;
             req.hostId = wf.SERVERS[srv].HOSTMAP[req.host];
-            req.HOST = wf.SERVERS[srv].HOSTS[wf.SERVERS[srv].HOSTMAP[req.host]];
+			req.SERVER = wf.SERVERS[srv];
+            req.HOST = req.SERVER.HOSTS[wf.SERVERS[srv].HOSTMAP[req.host]];
             req.app = req.HOST.appArray;
             wf.LoopExec(req, res);
         }
@@ -40,7 +41,8 @@ function HttpServer(srv)
             req.srv = srv;
             req.srvId = srv;
             req.hostId = wf.SERVERS[srv].HOSTS[wf.SERVERS[srv].HOSTMAP['*']];
-            req.HOST = wf.SERVERS[srv].HOSTS[wf.SERVERS[srv].HOSTMAP['*']];
+            req.SERVER = wf.SERVERS[srv];
+            req.HOST = req.SERVER.HOSTS[wf.SERVERS[srv].HOSTMAP['*']];
             req.app = wf.SERVERS[srv].HOSTS[wf.SERVERS[srv].HOSTMAP['*']].appArray;
             // START ENGINE/APP LOOP
             wf.LoopExec(req, res);
